648922f0289effadb5b201df3543b37bee2fa7ea
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-10-19 13:53:44 +0700

    add rotating header nav items

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -38,12 +38,19 @@ const Links: NavLink[] = [
 
 export default function Header() {
     const [currentIndex, setCurrentIndex] = useState(0)
+    const [isRotating, setIsRotating] = useState(false)
 
     const handleClickShutter = () => {
-        if (currentIndex <= Links.length - 2)
-            setCurrentIndex(currentIndex + 1)
-        else
-            setCurrentIndex(0)
+        setIsRotating(true)
+        setTimeout(() => {
+            if (currentIndex <= Links.length - 2)
+                setCurrentIndex(currentIndex + 1)
+            else
+                setCurrentIndex(0)
+        }, 500)
+        setTimeout(() => {
+            setIsRotating(false)
+        }, 1000)
     }
     return (
         <div className="w-[1440] mx-auto relative h-230">
@@ -58,15 +65,26 @@ export default function Header() {
             </div>
             {/* clickable shutter dial */}
             <div className="size-34 rounded-full hover:bg-white/10 hover:scale-110 hover:backdrop-blur-xs absolute top-62/100 left-47/100 z-3 transition-all duration-300" onClick={handleClickShutter}> </div>
-            <div className={`${instrument_Serif.className} flex flex-col justify-end items-end absolute z-2 top-39/100 right-20 h-91 w-254 bg-[#E0E1E1] text-right text-[56px] pr-4 pb-14 `}>
+            <div className={`${instrument_Serif.className} flex flex-col justify-end items-end absolute z-2 top-39/100 right-20 h-91 w-254 bg-[#E0E1E1] text-right text-[56px] pr-4 pb-14 overflow-hidden`}>
                 {Links[currentIndex].link ? (
-                    <Link href={Links[currentIndex].link}>{Links[currentIndex].text}</Link>
-                ) :
+                    <div className={isRotating ? 'rotating' : ''}>
+                        <Link href={Links[currentIndex].link}>{Links[currentIndex].text}</Link>
+                    </div>) :
 
-                    <> {Links[currentIndex].text}
-                        < Image src={'/Vector.png'} alt="" width={500} height={40} /></>
+                    <div className={isRotating ? 'rotating' : ''}> {Links[currentIndex].text}
+                        < Image src={'/Vector.png'} alt="" width={500} height={40} /></div>
                 }
             </div>
+            <style jsx>{`
+                .rotating {
+                    animation: rotateAroundShutter 1s ease-in-out;
+                    transform-origin: -489px 53px;
+                }
+                @keyframes rotateAroundShutter {
+                    0% { transform: rotate(0deg); }
+                    100% { transform: rotate(360deg); }
+                }
+            `}</style>
             {/* image */}
             <Image className="absolute top-0 right-0 z-1" src={'/header-bg.png'} width={700} height={741} alt=""></Image>
         </div >
